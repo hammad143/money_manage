1f10256024ef9b9f5c7b66b6054ba2760c546853
hammad143 <hammadpervez6@gmail.com>
2020-11-14 15:42:16 +0500

    perfect notification

--- lib/viewmodel/bloc/notifier_item_added_bloc/notifier_item_added_bloc.dart
+++ lib/viewmodel/bloc/notifier_item_added_bloc/notifier_item_added_bloc.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart'
+    as notification;
 import 'package:money_management/viewmodel/bloc/notifier_item_added_bloc/notifier_item_added_event.dart';
 import 'package:money_management/viewmodel/bloc/notifier_item_added_bloc/notifier_item_added_state.dart';
 
@@ -18,17 +20,32 @@ class NotifierItemAddedBloc
         final data = element.data();
         return data['auto_increment'] == documents.length;
       });
-      /*  final notificationPlugin = notification.FlutterLocalNotificationsPlugin();
-      final initSettings = notification.AndroidInitializationSettings(null);
-      await notificationPlugin.initialize(
-          notification.InitializationSettings(android: initSettings));
-      final andoridNotificaitonDetails =
-          notification.AndroidNotificationDetails(
-              "chennal", "channalname", "channal desc");
-      final n =
-          notification.NotificationDetails(android: andoridNotificaitonDetails);
-      notificationPlugin.show(0, "My test", "My try", n);*/
-      print("This is may Last added Document ${lastAddedDocument.data()}");
+      if (lastAddedDocument != null) {
+        final data = lastAddedDocument.data();
+        final androidInitSetting =
+            notification.AndroidInitializationSettings('app_icon');
+        final initSettings =
+            notification.InitializationSettings(android: androidInitSetting);
+        final plugin = notification.FlutterLocalNotificationsPlugin();
+        await plugin.initialize(initSettings);
+        final notificationChannelDetails =
+            notification.AndroidNotificationDetails("my_channel_id",
+                "Channel Name", "Here is the channel description",
+                priority: notification.Priority.high,
+                importance: notification.Importance.max,
+                enableVibration: true,
+                fullScreenIntent: true,
+                indeterminate: true,
+                maxProgress: 70);
+        final platformNotification = notification.NotificationDetails(
+            android: notificationChannelDetails);
+        plugin.show(
+            0,
+            "${data['title']}",
+            "Amount ${data['amount']}, Option: ${data['option']} added at: ${data['date']}",
+            platformNotification);
+        print("This is may Last added Document ${lastAddedDocument.data()}");
+      }
     }
   }
 }
